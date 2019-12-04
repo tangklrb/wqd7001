ea9546dbb2472360e5b070a5e44be34b66d139ef
Tang Kit Lim <tangklrb@yahoo.com>
2019-12-04 21:16:11 +0800

    Blank setup

---
+++ server.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(dplyr)
+library(formattable)
+library(ggplot2)
+
+shinyServer(
+  function(input, output) {
+  }
+)
---
+++ ui.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    titlePanel("WQD7001 - Group Project"),
+    sidebarLayout(position = "left",
+      sidebarPanel(
+      ),
+      mainPanel(
+      )
+    )
+  )
+)
